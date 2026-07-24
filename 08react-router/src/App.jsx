c094d6f5943cd82160bfcfa2a6d2f12fca348a1f
vikasvkori1290 <vikasvkori129@gmail.com>
2026-07-24 21:13:52 +0530

    react router dom

---
+++ 08react-router/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default App
---
+++ 08react-router/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+    const [data,setdata]=useState({})
+    useEffect(()=>{
+        
+        fetch('https://api.github.com/users/vikasvkori1290')
+        .then(response => response.json())
+        .then(data=>{
+            console.log(data)
+            setdata(data)
+        })
+    },[])
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers : {data.followers}
+    <img src={data.avatar_url} alt="" width="123"/>
+    </div>
+  )
+}
+
+export default Github   
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/vikasvkori1290')
+    return response.json()
+}
